321a4e0412b3852d012dcef0cc589cfe8c6ef6c2
Diway Sanu <diway.sanu@team.telstra.com>
2020-02-18 08:45:28 +1100

    First commit

---
+++ src/main/java/com/microservices/movieinfoservice/controllers/MovieController.java
@@ -0,0 +1,20 @@
+package com.microservices.movieinfoservice.controllers;
+
+import com.microservices.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+
+        return new Movie(movieId, "Transformers");
+    }
+}
